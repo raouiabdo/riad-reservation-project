1961cf4080cebc3991736cbde7e37e327b4837b7
Anas <himdo368@gmail.com>
2024-12-02 23:10:17 +0100

    this is anouther commit from anas

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+// hello this test of adding from another divice
 Route::get('/', function () {
     return view('welcome');
 });
